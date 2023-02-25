44b735aa24e67ba93699c2d90cf8e7c29268cc9e
glh <glh@163.com>
2023-02-25 11:54:40 +0800

    我们-食品安全

--- dxc/src/views/Me/Introduction.vue
+++ dxc/src/views/Me/Introduction.vue
@@ -10,39 +10,249 @@
       历经两个多世纪的发展，一代代稻香村人的传承与创新，已成为闻名中外的大型现代化食品企业集团。
     </p>
 
-    <img src="../../assets/me/qyjs01.jpg" class="qyjs">
+    <img src="../../assets/me/qyjs01.jpg" class="qyjs" />
 
-    <div class="two">      
-      <img src="../../assets/me/qyjtfz.png" class="font">
+    <!-- f1  企业发展 -->
+    <div class="two">
+      <img src="../../assets/me/qyjtfz.png" class="font" />
     </div>
 
     <div class="qyjtfz">
       <div class="left">
-        <img src="../../assets/me/qyjs02.jpg" alt="">
+        <img src="../../assets/me/qyjs02.jpg" alt="" />
         <div class="wz">
           <p class="title">182.77亿</p>
           <p class="desc">2022中国品牌价值评估,稻香村品牌价值182.77亿元。</p>
-        </div> 
+        </div>
       </div>
       <div class="right">
         <ul>
           <li>
-            <img src="../../assets/me/qyjs03.jpg" alt="">
+            <img src="../../assets/me/qyjs03.jpg" alt="" />
             <div class="wz">
               <p class="title">7</p>
               <p class="desc">七大原料供应基地</p>
             </div>
           </li>
           <li>
-            <img src="../../assets/me/qyjs03.jpg" alt="">
+            <img src="../../assets/me/qyjs03.jpg" alt="" />
             <div class="wz">
               <p class="title">7</p>
               <p class="desc">七大原料供应基地</p>
+            </div>
+          </li>
+          <li>
+            <img src="../../assets/me/qyjs04.jpg" alt="" />
+            <div class="wz">
+              <p class="title">10</p>
+              <p class="desc">十大现代化食品生产加工中心</p>
+            </div>
+          </li>
+          <li>
+            <img src="../../assets/me/qyjs04.jpg" alt="" />
+            <div class="wz">
+              <p class="title">1500亩</p>
+              <p class="desc">总占地面积1500亩</p>
+            </div>
+          </li>
+          <li>
+            <img src="../../assets/me/qyjs05.jpg" alt="" />
+            <div class="wz">
+              <p class="title">3000</p>
+              <p class="desc">拥有员工3000多人</p>
+            </div>
+          </li>
+          <li>
+            <img src="../../assets/me/qyjs06.jpg" alt="" />
+            <div class="wz">
+              <p class="title">800家</p>
+              <p class="desc">在全国有800多家专卖专营店</p>
             </div>
           </li>
         </ul>
       </div>
     </div>
+
+    <!-- f2  互联网 -->
+    <div class="two">
+      <img src="../../assets/me/hlw-wz.jpg" class="font" />
+    </div>
+
+    <p>
+      目前稻香村集团在全国有800多家专卖专营店，覆盖全国大部分区域。并以每年近百家的速度增长。
+      传统制造企业“互联网+”已成为必需，稻香村集团是老字号较早一批“触网”的企业，
+      2009年就确立并开始实施了“互联网+稻香村”战略，成立了电子商务部，
+      到目前已经连续多年在天猫、京东等主流电商平台糕点类目、月饼类目取得骄人的成绩。
+      电商渠道的销售占比逐年提高，目前已近30%。
+    </p>
+
+    <ul class="hlw">
+      <li>
+        <img src="../../assets/me/qyjs07.jpg" alt="" />
+        <div class="wz">
+          <p class="title">800多家专卖专营店</p>
+          <p class="desc">
+            目前稻香村集团在全国有800多家专卖专营店，覆盖全国大部分区域。
+          </p>
+        </div>
+      </li>
+      <li>
+        <img
+          src="http://www.daoxiangcun.cn/UpLoadFile/Images/2016/12/20/93737674a67f71f4-5_cut400554.jpg"
+          alt=""
+        />
+        <div class="wz">
+          <p class="title">互联网+</p>
+          <p class="desc">
+            传统制造企业“互联网+”已成为必需，2009年确立并开始实施了“互联网+稻香村”战略，
+            成立了电子商务部，到目前已经连续多年在天猫、京东等主流电商平台糕点类目、月饼类目取得骄人的成绩。
+          </p>
+        </div>
+      </li>
+      <li>
+        <img
+          src="http://www.daoxiangcun.cn/UpLoadFile/Images/2016/12/20/938134920da67ec4-6_cut400554.jpg"
+          alt=""
+        />
+        <div class="wz">
+          <p class="title">30%</p>
+          <p class="desc">电商渠道的市场销售占比逐年提高，目前已近30%。</p>
+        </div>
+      </li>
+    </ul>
+
+    <!-- f3  现代科技 -->
+    <div class="two">
+      <img src="../../assets/me/xdkj-wz.png" class="font" />
+    </div>
+
+    <p>
+      稻香村集团拥有120余条国内外现代化生产线，并先后通过QS、GMP、HACCP、ISO9001等多项认证，十万级空气净化生产车间，
+      生产过程全程监控，全方位透明化无菌生产，现代化的生产设备及工艺与科学管理体系，使得稻香村悠久文化与现代加工技术融为一体。
+    </p>
+
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <div class="grid-content bg-purple">
+          <img src="../../assets/me/qyjs08.jpg" alt="">
+        </div>
+      </el-col>
+      <el-col :span="16">
+        <div class="grid-content bg-purple">
+          <img src="../../assets/me/qyjs09.jpg" alt="">
+        </div>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="20">
+      <el-col :span="16">
+        <div class="grid-content bg-purple">
+          <img src="../../assets/me/qyjs10.jpg" alt="">
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="grid-content bg-purple">
+          <img src="../../assets/me/qyjs11.jpg" alt="">
+        </div>
+      </el-col>
+    </el-row>
+
+    <!-- 现代科技 -->
+    <div class="two">
+      <img src="../../assets/me/kjcx-wz.jpg" class="font" />
+    </div>
+
+    <p>
+      食品企业新技术、新产品的研发能力是打造核心竞争力的基础，2015年稻香村集团在研发上投入2000多万元，
+      并保持每年近20%的速度增长，稻香村集团依托江南大学、国家食品科研和质量监督检验部门的强大技术能力，
+      联合成立"中国糕点食品技术研发中心"、"福临门联合烘焙研究所"，“苏州工业园区稻香村博士后孵化站” 等产学研合作平台，
+      紧跟国际最新食品科技进行自主创新，重点致力于焙烤冷冻面团发酵过程优化关键技术,与系列新产品、新技术研究与开发，
+      并以"中国传统食品工业化加工关键技术"为主题突破口进行集成创新，引进欧美先进技术和装备，
+      打造并提升“稻香村”精品品牌形象，引领行业技术跨越式发展。
+    </p>
+
+    <!-- 食品安全 -->
+    <div class="two">
+      <img src="../../assets/me/spaq-wz.jpg" class="font" />
+    </div>
+
+    <el-row>
+      <el-col :span="6">
+        <div class="grid-content">
+          <img src="../../assets/me/qyjs12.jpg" alt="">
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content">
+          <div class="wz spaq">
+            <p class="title">
+              2
+            </p>
+            <p class="desc">
+              车间设有108个摄像监控，16处IC自动门禁，8道隔离门
+            </p>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content">
+          <img src="../../assets/me/qyjs14.jpg" alt="">
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content">
+          <div class="wz spaq">
+            <p class="title">
+              4
+            </p>
+            <p class="desc">
+              生产员工到岗需经过7次检查、除尘消毒、通过8道隔离门
+            </p>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="6">
+        <div class="grid-content">
+          <div class="wz spaq">
+            <p class="title">
+              2
+            </p>
+            <p class="desc">
+              车间设有108个摄像监控，16处IC自动门禁，8道隔离门
+            </p>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <img src="../../assets/me/qyjs13.jpg" alt="">
+        <div class="grid-content">
+
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content">
+          <div class="wz spaq">
+            <p class="title">
+              4
+            </p>
+            <p class="desc">
+              生产员工到岗需经过7次检查、除尘消毒、通过8道隔离门
+            </p>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content">
+          <img src="../../assets/me/qyjs15.jpg" alt="">  
+        </div>
+      </el-col>
+    </el-row>
+
+
+
 
 
   </div>
@@ -53,83 +263,85 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.qyjs{
+.qyjs {
   margin: 20px 0px;
 }
-.bj1{
+.bj1 {
   width: 100%;
   height: 173px;
-  background-image: url('../../assets/me/bj1.jpg');
+  background-image: url("../../assets/me/bj1.jpg");
   background-size: contain;
 }
-.two{
+.two {
   width: 100%;
   height: 173px;
-  background-image: url('../../assets/me/jgc-bj.jpg');
-  background-size: contain; 
+  background-image: url("../../assets/me/jgc-bj.jpg");
+  background-size: contain;
   background-repeat: no-repeat;
   display: flex;
 }
 // 图片居中,父弹性布局,子margin:auto
-.font{
+.font {
   width: 300px;
   margin: auto;
 }
 
 //企业集团发展
-.qyjtfz{
+.qyjtfz {
   display: flex;
-  .left{
+  margin-bottom: 40px;
+  .left {
     width: 700px;
     height: 600px;
     position: relative;
-    img{
+    img {
       width: 100%;
       height: 100%;
       object-fit: cover;
     }
-    .wz{
+    .wz {
       display: flex;
       flex-direction: column;
       position: absolute;
       left: 0;
       bottom: 0;
       padding: 30px;
-      .title{
+      .title {
         font-size: 72px;
         color: #fff;
       }
-      .desc{
+      .desc {
         font-size: 16px;
         color: #fff;
       }
     }
-
   }
-  .right{
+  .right {
     width: 500px;
-    border: 1px solid red;
-    ul{
+    ul {
       display: flex;
-      li{
+      flex-direction: row;
+      flex-wrap: wrap;
+      li {
         width: 250px;
         height: 200px;
         position: relative;
-        img{
+        img {
           width: 100%;
+          height: 200px;
         }
-        .wz{
+        .wz {
           display: flex;
           flex-direction: column;
           position: absolute;
           left: 0;
           bottom: 0;
           padding: 20px;
-          .title{
+          .title {
             font-size: 48px;
             color: #fff;
           }
-          .desc{
+          .desc {
             font-size: 14px;
             color: #fff;
           }
@@ -138,4 +350,56 @@ export default {};
     }
   }
 }
+
+//互联网
+.hlw {
+  display: flex;
+  margin: 40px 0;
+  li {
+    width: calc(100% / 3);
+    height: 554px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+    .wz {
+      width: 300px;
+      position: absolute;
+      top: 350px;
+      left: 50px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .title {
+        font-size: 24px;
+        color: #fff;
+      }
+      .desc {
+        font-size: 16px;
+        color: #fff;
+      }
+    }
+  }
+}
+
+//现代科技
+.el-row {
+  margin-top: 20px;
+  &:last-child {
+    margin-bottom: 20px;
+  }
+}
+.grid-content {
+  border-radius: 4px;
+  overflow: hidden;
+  img{
+    width: 100%;
+    object-fit: cover;
+  }
+}
+
+
+
 </style>
