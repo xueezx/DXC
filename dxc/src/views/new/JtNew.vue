8aca3014e35cab9bd80fdd0748e1bcb7649a14d4
郭晨鹏 <22926069967@qq.com>
2023-03-10 09:46:49 +0800

    新闻页面跳转

--- dxc/src/views/new/JtNew.vue
+++ dxc/src/views/new/JtNew.vue
@@ -38,12 +38,12 @@
         :key="item.id"
       >
         <div class="left2">
-          <img :src="item.avatar" alt="" />
+          <img :src="item.pic" alt="" />
         </div>
         <div class="right2">
           <h4>{{ item.title }}</h4>
           <p>{{ item.time }}</p>
-          <span>{{ item.detail }}</span>
+          <span>{{ item.content }}</span>
           <div class="btn">请进</div>
         </div>
       </div>
@@ -59,35 +59,6 @@
       @current-change="changeCurrentPage"
     ></el-pagination>
   </div>
-
-  <div class="bt1">
-    <img src="../../assets/jtnew.jpg" alt="" />
-  </div>
-  <router-view />
-  <div class="hos" v-show="hideOrShow">
-    <!-- 分页遍历 -->
-    <!-- $router.push('/home/new/jtnew/jt/' + item.id) -->
-    <div class="jump2" v-for="item in news" :key="item.id">
-      <div class="left2">
-        <img :src="item.pic" alt="" />
-      </div>
-      <div class="right2">
-        <h4>{{ item.title }}</h4>
-        <p>{{ item.time }}</p>
-        <span>{{ item.content }}</span>
-        <div class="btn" @click="$router.push('/home/new/jtnew/jt/' + item.id)">
-          请进
-        </div>
-      </div>
-    </div>
-  </div>
-  <el-pagination
-    style="background-color: #ceaf59"
-    background
-    layout="prev, pager, next"
-    :total="1000"
-  >
-  </el-pagination>
 </template>
 
 <script>
@@ -97,33 +68,64 @@ export default {
   data() {
     return {
       hideOrShow: true,
-      news: [],
-      id: 1,
-      page: 1,
-      pagesize: 10,
+      news: [
+        // {
+        //     id: 1,
+        //     avatar: 'http://www.daoxiangcun.cn/UpLoadFile/Images/2023/2/6/17237326c438096-a_Cut285180.jpg',
+        //     title: '稻香村荣登新国货品牌百强，获“最受消费者欢迎新国货品牌”大奖。',
+        //     time: 'Feb 06,2023',
+        //     detail: '近日，由全球知名的新经济产业第三方数据挖掘和分析机构iiMedia Research（艾媒咨询）主办的艾媒新消费大讲堂第6期暨2022年中国新国货品牌“金榜题名”颁奖典礼成功举办并揭晓获奖名单。稻香村荣获“2022年中国最受消费者欢迎新国货品牌”大奖，并入'
+        // },
+        // {
+        //     id: 2,
+        //     avatar: 'http://www.daoxiangcun.cn/UpLoadFile/Images/2022/9/28/1118457091350d3c9-7_Cut285180.jpg',
+        //     title: '稻香村荣登新国货品牌百强，获“最受消费者欢迎新国货品牌”大奖。',
+        //     time: 'Feb 07,2023',
+        //     detail: '近日，由全球知名的新经济产业第三方数据挖掘和分析机构iiMedia Research（艾媒咨询）主办的艾媒新消费大讲堂第6期暨2022年中国新国货品牌“金榜题名”颁奖典礼成功举办并揭晓获奖名单。稻香村荣获“2022年中国最受消费者欢迎新国货品牌”大奖，并入'
+        // },
+        // {
+        //     id: 3,
+        //     avatar: 'http://www.daoxiangcun.cn/UpLoadFile/Images/2023/2/6/17237326c438096-a_Cut285180.jpg',
+        //     title: '稻香村荣登新国货品牌百强，获“最受消费者欢迎新国货品牌”大奖。',
+        //     time: 'Feb 08,2023',
+        //     detail: '近日，由全球知名的新经济产业第三方数据挖掘和分析机构iiMedia Research（艾媒咨询）主办的艾媒新消费大讲堂第6期暨2022年中国新国货品牌“金榜题名”颁奖典礼成功举办并揭晓获奖名单。稻香村荣获“2022年中国最受消费者欢迎新国货品牌”大奖，并入'
+        // },
+      ],
+      newsData: {
+        // 保存电影数据
+        page: 1, // 当前页码
+        pagesize: 10, // 每页多少条
+        total: 999, // 总条目数
+        xwfl_id: 1, //新闻类型id
+        result: [], // 电影列表
+      },
     }
   },
   methods: {
     detail(id) {
-      this.$router.push('/home/new/jtnew/jt/' + id)
+      this.$router.push('/home/new/xwfl_id/1/jt/' + id)
       if (id) {
         this.hideOrShow = false
       }
     },
-    init() {
+    changeCurrentPage(page) {
+      this.newsData.page = page // 修改当前页
+      this.queryMovies() // 重新加载当前页的新闻列表
+    },
+    upload() {
       let params = {
-        xwfl_id: this.id,
-        page: this.page,
-        pagesize: this.pagesize,
+        page: this.newsData.page,
+        pagesize: this.newsData.pagesize,
+        xwfl_id: this.newsData.xwfl_id,
       }
       httpApi.newsApi.queryNewsByPage(params).then(res => {
-        console.log(res)
+        console.log('新闻列表', res)
         this.news = res.data.data
       })
     },
   },
   mounted() {
-    this.init()
+    this.upload()
   },
 }
 </script>
--- dxc/src/views/new/MtNew.vue
+++ dxc/src/views/new/MtNew.vue
@@ -1,225 +1,247 @@
 <template>
-    <div class="mt">
-        <!-- 面包屑导航 -->
-        <div class="breadcrumb" style="min-width:320px">
-            <span style="margin-left:20px;line-height:100%" class="wz">您现在的位置：</span>
-            <el-breadcrumb style="margin:0 0 0 20px;display:inline-block" separator-class="el-icon-arrow-right">
-                <el-breadcrumb-item :to="{ path: '/' }" class="wz">首页</el-breadcrumb-item>
-                <el-breadcrumb-item class="wz">新闻</el-breadcrumb-item>
-                <el-breadcrumb-item><span style="color:red" class="wz">媒体报道</span></el-breadcrumb-item>
-            </el-breadcrumb>
-        </div>
-
-        <div class="bt1">
-            <img src="../../assets/mtnew.jpg" alt="">
-        </div>
-
-        <router-view />
-        <div class="hos" v-show="hideOrShow">
-            <!-- 分页遍历 -->
-            <div @click="detail(item.id)" class="jump2" v-for="item in news" :key="item.id">
-                <div class="left2">
-                    <img :src="item.avatar" alt="">
-                </div>
-                <div class="right2">
-                    <h4>{{ item.title }}</h4>
-                    <p>{{ item.time }}</p>
-                    <span>{{ item.detail }}</span>
-                    <div class="btn">请进</div>
-                </div>
-            </div>
-        </div>
+  <div class="mt">
+    <!-- 面包屑导航 -->
+    <div class="breadcrumb" style="min-width: 320px">
+      <span style="margin-left: 20px; line-height: 100%" class="wz"
+        >您现在的位置：</span
+      >
+      <el-breadcrumb
+        style="margin: 0 0 0 20px; display: inline-block"
+        separator-class="el-icon-arrow-right"
+      >
+        <el-breadcrumb-item :to="{ path: '/' }" class="wz"
+          >首页</el-breadcrumb-item
+        >
+        <el-breadcrumb-item class="wz">新闻</el-breadcrumb-item>
+        <el-breadcrumb-item
+          ><span style="color: red" class="wz"
+            >媒体报道</span
+          ></el-breadcrumb-item
+        >
+      </el-breadcrumb>
     </div>
+
+    <div class="bt1">
+      <img src="../../assets/mtnew.jpg" alt="" />
+    </div>
+
+    <router-view />
+    <div class="hos" v-show="hideOrShow">
+      <!-- 分页遍历 -->
+      <div
+        @click="detail(item.id)"
+        class="jump2"
+        v-for="item in news"
+        :key="item.id"
+      >
+        <div class="left2">
+          <img :src="item.avatar" alt="" />
+        </div>
+        <div class="right2">
+          <h4>{{ item.title }}</h4>
+          <p>{{ item.time }}</p>
+          <span>{{ item.detail }}</span>
+          <div class="btn">请进</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            hideOrShow: true,
-            news: [
-                {
-                    id: 1,
-                    avatar: 'http://www.daoxiangcun.cn/UpLoadFile/Images/2023/2/6/17237326c438096-a_Cut285180.jpg',
-                    title: '稻香村荣登新国货品牌百强，获“最受消费者欢迎新国货品牌”大奖。',
-                    time: 'Feb 06,2023',
-                    detail: '近日，由全球知名的新经济产业第三方数据挖掘和分析机构iiMedia Research（艾媒咨询）主办的艾媒新消费大讲堂第6期暨2022年中国新国货品牌“金榜题名”颁奖典礼成功举办并揭晓获奖名单。稻香村荣获“2022年中国最受消费者欢迎新国货品牌”大奖，并入'
-                },
-                {
-                    id: 2,
-                    avatar: 'http://www.daoxiangcun.cn/UpLoadFile/Images/2022/9/28/1118457091350d3c9-7_Cut285180.jpg',
-                    title: '稻香村荣登新国货品牌百强，获“最受消费者欢迎新国货品牌”大奖。',
-                    time: 'Feb 07,2023',
-                    detail: '近日，由全球知名的新经济产业第三方数据挖掘和分析机构iiMedia Research（艾媒咨询）主办的艾媒新消费大讲堂第6期暨2022年中国新国货品牌“金榜题名”颁奖典礼成功举办并揭晓获奖名单。稻香村荣获“2022年中国最受消费者欢迎新国货品牌”大奖，并入'
-                },
-                {
-                    id: 3,
-                    avatar: 'http://www.daoxiangcun.cn/UpLoadFile/Images/2023/2/6/17237326c438096-a_Cut285180.jpg',
-                    title: '稻香村荣登新国货品牌百强，获“最受消费者欢迎新国货品牌”大奖。',
-                    time: 'Feb 08,2023',
-                    detail: '近日，由全球知名的新经济产业第三方数据挖掘和分析机构iiMedia Research（艾媒咨询）主办的艾媒新消费大讲堂第6期暨2022年中国新国货品牌“金榜题名”颁奖典礼成功举办并揭晓获奖名单。稻香村荣获“2022年中国最受消费者欢迎新国货品牌”大奖，并入'
-                },
-            ]
-        }
-    },
-    methods: {
-        detail(id) {
-            this.$router.push('/home/new/mtnew/mt/' + id)
-            if (id) {
-                this.hideOrShow = false
-            } else {
-                this.hideOrShow = true
-            }
-        }
+  data() {
+    return {
+      hideOrShow: true,
+      news: [
+        {
+          id: 1,
+          avatar:
+            'http://www.daoxiangcun.cn/UpLoadFile/Images/2023/2/6/17237326c438096-a_Cut285180.jpg',
+          title: '稻香村荣登新国货品牌百强，获“最受消费者欢迎新国货品牌”大奖。',
+          time: 'Feb 06,2023',
+          detail:
+            '近日，由全球知名的新经济产业第三方数据挖掘和分析机构iiMedia Research（艾媒咨询）主办的艾媒新消费大讲堂第6期暨2022年中国新国货品牌“金榜题名”颁奖典礼成功举办并揭晓获奖名单。稻香村荣获“2022年中国最受消费者欢迎新国货品牌”大奖，并入',
+        },
+        {
+          id: 2,
+          avatar:
+            'http://www.daoxiangcun.cn/UpLoadFile/Images/2022/9/28/1118457091350d3c9-7_Cut285180.jpg',
+          title: '稻香村荣登新国货品牌百强，获“最受消费者欢迎新国货品牌”大奖。',
+          time: 'Feb 07,2023',
+          detail:
+            '近日，由全球知名的新经济产业第三方数据挖掘和分析机构iiMedia Research（艾媒咨询）主办的艾媒新消费大讲堂第6期暨2022年中国新国货品牌“金榜题名”颁奖典礼成功举办并揭晓获奖名单。稻香村荣获“2022年中国最受消费者欢迎新国货品牌”大奖，并入',
+        },
+        {
+          id: 3,
+          avatar:
+            'http://www.daoxiangcun.cn/UpLoadFile/Images/2023/2/6/17237326c438096-a_Cut285180.jpg',
+          title: '稻香村荣登新国货品牌百强，获“最受消费者欢迎新国货品牌”大奖。',
+          time: 'Feb 08,2023',
+          detail:
+            '近日，由全球知名的新经济产业第三方数据挖掘和分析机构iiMedia Research（艾媒咨询）主办的艾媒新消费大讲堂第6期暨2022年中国新国货品牌“金榜题名”颁奖典礼成功举办并揭晓获奖名单。稻香村荣获“2022年中国最受消费者欢迎新国货品牌”大奖，并入',
+        },
+      ],
     }
+  },
+  methods: {
+    detail(id) {
+      this.$router.push('/home/new/xwfl_id/3/mt/' + id)
+      if (id) {
+        this.hideOrShow = false
+      } else {
+        this.hideOrShow = true
+      }
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .mt {
-    width: 100%;
-    background-color: #FDFAF5;
+  width: 100%;
+  background-color: #fdfaf5;
 }
 
 .breadcrumb {
-    max-width: 1200px;
-    margin: auto;
-    padding-top: 20px;
-    .wz{
-        font-weight: normal;
-        font-size: 14px;
-        color: #333;
-    }
+  max-width: 1200px;
+  margin: auto;
+  padding-top: 20px;
+  .wz {
+    font-weight: normal;
+    font-size: 14px;
+    color: #333;
+  }
 }
 
 .bt1 {
-    background-image: url('../../assets/bt1.jpg');
-    background-size: 100% 100px;
-    text-align: center;
+  background-image: url('../../assets/bt1.jpg');
+  background-size: 100% 100px;
+  text-align: center;
 }
 
-.bt1>img {
-    width: 120px;
-    margin: 35px 0;
+.bt1 > img {
+  width: 120px;
+  margin: 35px 0;
 }
 
 .hos {
-    .jump2 {
-        display: flex;
-        width: 1200px;
-        margin: 20px auto;
+  .jump2 {
+    display: flex;
+    width: 1200px;
+    margin: 20px auto;
 
-        .right2 {
-            background-color: #F9F4E4;
-            width: 57%;
-            padding: 20px;
+    .right2 {
+      background-color: #f9f4e4;
+      width: 57%;
+      padding: 20px;
 
-            >h4 {
-                color: #C28F39;
-                font-size: 16px;
-                font-weight: lighter;
+      > h4 {
+        color: #c28f39;
+        font-size: 16px;
+        font-weight: lighter;
 
-                &:hover {
-                    color: rgb(231, 77, 77);
-                }
-            }
-
-            >span {
-                display: inline-block;
-            }
-
-            >p {
-                margin: 10px 0;
-                color: #C28F39;
-                font-size: 12px;
-            }
-
-            .btn {
-                margin: 20px 0 0 600px;
-                font-size: 16px;
-                color: white;
-                text-align: center;
-                line-height: 45px;
-                background-color: #BFA764;
-                width: 45px;
-                height: 45px;
-                border-radius: 100%;
-
-                &:hover {
-                    color: rgb(231, 77, 77);
-                }
-            }
+        &:hover {
+          color: rgb(231, 77, 77);
         }
+      }
 
-        .left2 {
-            width: 43%;
-            overflow: hidden;
+      > span {
+        display: inline-block;
+      }
 
-            >img {
-                display: block;
-                width: 100%;
-                height: 250px;
-            }
+      > p {
+        margin: 10px 0;
+        color: #c28f39;
+        font-size: 12px;
+      }
+
+      .btn {
+        margin: 20px 0 0 600px;
+        font-size: 16px;
+        color: white;
+        text-align: center;
+        line-height: 45px;
+        background-color: #bfa764;
+        width: 45px;
+        height: 45px;
+        border-radius: 100%;
+
+        &:hover {
+          color: rgb(231, 77, 77);
         }
+      }
     }
 
-    :first-child {
-        flex-direction: row-reverse;
+    .left2 {
+      width: 43%;
+      overflow: hidden;
 
-        .left2 {
-            width: 650px;
-            overflow: hidden;
-
-            >img {
-                display: block;
-                width: 100%;
-                height: 350px;
-            }
-        }
-
-        .right2 {
-            background-color: #F9F4E4;
-            height: 290px;
-            width: 550px;
-            padding: 30px;
-
-            >h4 {
-                color: #C28F39;
-                font-size: 20px;
-                font-weight: lighter;
-
-                &:hover {
-                    color: rgb(231, 77, 77);
-                }
-            }
-
-            >span {
-                display: inline-block;
-                font-size: 20px;
-            }
-
-            >p {
-                margin: 10px 0;
-                color: #C28F39;
-                font-size: 16px;
-            }
-
-            .btn {
-                font-size: 20px;
-                color: white;
-                text-align: center;
-                line-height: 60px;
-                background-color: #BFA764;
-                width: 60px;
-                height: 60px;
-                border-radius: 100%;
-                margin-left: 10px;
-
-                &:hover {
-                    color: rgb(231, 77, 77);
-                }
-            }
-        }
+      > img {
+        display: block;
+        width: 100%;
+        height: 250px;
+      }
     }
+  }
+
+  :first-child {
+    flex-direction: row-reverse;
+
+    .left2 {
+      width: 650px;
+      overflow: hidden;
+
+      > img {
+        display: block;
+        width: 100%;
+        height: 350px;
+      }
+    }
+
+    .right2 {
+      background-color: #f9f4e4;
+      height: 290px;
+      width: 550px;
+      padding: 30px;
+
+      > h4 {
+        color: #c28f39;
+        font-size: 20px;
+        font-weight: lighter;
+
+        &:hover {
+          color: rgb(231, 77, 77);
+        }
+      }
+
+      > span {
+        display: inline-block;
+        font-size: 20px;
+      }
+
+      > p {
+        margin: 10px 0;
+        color: #c28f39;
+        font-size: 16px;
+      }
+
+      .btn {
+        font-size: 20px;
+        color: white;
+        text-align: center;
+        line-height: 60px;
+        background-color: #bfa764;
+        width: 60px;
+        height: 60px;
+        border-radius: 100%;
+        margin-left: 10px;
+
+        &:hover {
+          color: rgb(231, 77, 77);
+        }
+      }
+    }
+  }
 }
 </style>
